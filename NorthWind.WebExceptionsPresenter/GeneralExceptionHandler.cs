50e69c89e7e6ef3f219dc049a06b288ce2c5fcca
AlexGuzmanHurtado <alexguzmanxp12@gmail.com>
2024-04-13 02:18:39 -0500

    Exception Handler

---
+++ NorthWind.WebExceptionsPresenter/GeneralExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NorthWind.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.WebExceptionsPresenter
+{
+	public class GeneralExceptionHandler : ExceptionHandlerBase, IExceptionHandler
+	{
+		public Task Handle(ExceptionContext context)
+		{
+			var Exception = context.Exception as GeneralExceptions;
+			return SetResult(context, StatusCodes.Status500InternalServerError,
+				Exception.Message, Exception.Details);
+		}
+	}
+	{
+
+	}
+}
